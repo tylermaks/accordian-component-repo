d14fabfa194d0e5c94448bf114eca82f0a6b7084
Tyler Maksymiw <tylermcodes@gmail.com>
2022-07-10 21:38:52 -0400

    Initial commit

---
+++ src/AccordianCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AccordianCard (){
+    return(
+        <div className="accordian-card row">
+            <div className="col left-column">
+                <img className="fg-img img-fluid" src="images/illustration-woman-online-desktop.svg" alt="Woman looking at computer" />
+                
+            </div>
+            <div className="col">
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default AccordianCard;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+import AccordianCard from "./AccordianCard"
+
+function App() {
+  return (
+    <div className="App">
+      <AccordianCard />
+    </div>
+  );
+}
+
+export default App;
