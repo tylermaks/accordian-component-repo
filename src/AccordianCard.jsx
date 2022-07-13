8637ac68bdbf7a48da56a9914c9510e84fe5ea96
Tyler Maksymiw <tylermcodes@gmail.com>
2022-07-12 21:45:50 -0400

    Add responsive image to mobile sizing

--- src/AccordianCard.jsx
+++ src/AccordianCard.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 function AccordianCard (){
     return(
-        <div className="accordian-card row">
-            <div className="col left-column">
-                <img className="fg-img img-fluid" src="images/illustration-woman-online-desktop.svg" alt="Woman looking at computer" />
-                
+        <div className="container bg-light accordian-card">
+            <div className="row">
+                <div className="col-12 col-md-6 text-center img-col">
+                    <img className="mobile-img" src="images/illustration-woman-online-mobile.svg" alt="Woman using computer" /> 
+                    <img className="mobile-shadow" src="images/bg-pattern-mobile.svg" alt="shadow" />
+                </div>
             </div>
-            <div className="col">
-
-            </div>
-            
-        </div>
+        </div> 
     )
 }
 
